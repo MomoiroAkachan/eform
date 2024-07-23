fb5b04275f712aaf09faf39dfe4b55d6a84456dd
MomoiroAkachan <filiplemes@hotmail.com>
2024-07-23 09:56:16 -0300

    feat: create blade base

---
+++ resources/views/layout/blank.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="@yield('description')">
+    <title> {{ config('app.name') }}  - @yield('title')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
