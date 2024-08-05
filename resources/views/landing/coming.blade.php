3b72feb896788f1ecc072732aa9071fb3e67a1c0
MomoiroAkachan <filiplemes@hotmail.com>
2024-08-05 09:15:21 -0300

    style: finish 'coming' page

--- resources/views/landing/coming.blade.php
+++ resources/views/landing/coming.blade.php
@@ -10,19 +10,19 @@
 
         <div class="title">
             <h1>Cadastro</h1>
-            <h1 style='text-shadow: 0 0 3px #2227;'>escolar 2024/2025</h1>
+            <h1 style='text-shadow: 0 0 0.3rem #2227;'>escolar 2024/2025</h1>
         </div>
 
         <div class="subtitle">
             <h1>educação infantil</h1>
         </div>
 
-        <div class="infos relative mt-[20%] max-w-[80%] mx-auto max-h-[40%]">
+        <div class="infos">
             <div class="ratio">
                 <h4>Para crianças de 0 a 5 anos</h4>
             </div>
 
-            <div class="ctt text-[1.5rem] md:text-[3rem] shadow-lg border pt-[10%] border-gray-300 bg-white md:px-8 px-5 z-[10] relative py-7 rounded-md text-center">
+            <div class="book">
                 <h3>Cadastro disponível a partir do dia</h3>
                 <h3>
                     <span class='font-bold'>20/08</span> até <span class='font-bold'>20/09</span> de 2024
@@ -31,8 +31,8 @@
         </div>
 
         <div class="realization mt-[15%] flex flex-col items-center justify-center max-h-[20%]">
-            <h5 class='font-bold text-[1rem] md:text-[2.5rem]'>Realização:</h5>
-            <img src="{{ asset('img/logo-prefeitura.png') }}" alt="Logotipo da prefeitura" class="h-[8rem] md:h-[16rem]">
+            <h5 class='font-bold text-[1rem] md:text-[1.7rem]'>Realização:</h5>
+            <img src="{{ asset('img/logo-prefeitura.png') }}" alt="Logotipo da prefeitura" class="h-[8rem] md:h-[8rem]">
         </div>
     </div>
 </div>
