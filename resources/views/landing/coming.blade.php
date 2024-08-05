210dc8db1863ad992a646d9a1c6791f40249e169
MomoiroAkachan <filiplemes@hotmail.com>
2024-08-05 10:16:13 -0300

    style(coming): coming page adjusted

--- resources/views/landing/coming.blade.php
+++ resources/views/landing/coming.blade.php
@@ -6,7 +6,7 @@
 @endSection
 @section('content')
 <div class="bg">
-    <div class="context">
+    <div class="context relative">
 
         <div class="title">
             <h1>Cadastro</h1>
@@ -30,14 +30,15 @@
             </div>
         </div>
 
-        <div class="realization mt-[15%] flex flex-col items-center justify-center max-h-[20%] uppercase">
-            <h5 class='font-[700] md:font-[900] text-[1rem] md:text-[2rem]'>Realização:</h5>
-            <div class="flex flex-row gap-[10%] items-center justify-center mt-[5%]">
-                <span class="max-w-[40%] font-[800] md:font-[900] uppercase text-center text-[0.8rem] md:text-2xl text-slate-700">Secretaria de Educação</span>
+        <div class="realization mt-[8%] flex flex-col items-center uppercase lg:fixed lg:m-0 lg:right-0 lg:bottom-[15%]">
+            <h5 class='font-[700] md:font-[900] text-[1rem] md:text-[1.5rem]'>Realização:</h5>
+            <div class="flex flex-row gap-[10%] items-center justify-center mt-[5%] lg:mt-0">
+                <span class="max-w-[40%] font-[800] md:font-[900] uppercase text-center text-[0.8rem] md:text-2xl lg:text-xl text-slate-700">Secretaria de Educação</span>
                 <span class="font-[600] text-red-500">&</span>
-                <img src="{{ asset('img/logo-prefeitura.png') }}" alt="Logotipo da prefeitura" class="h-[5rem] md:h-[8rem]">
+                <img src="{{ asset('img/logo-prefeitura.png') }}" alt="Logotipo da prefeitura" class="h-[5rem] md:h-[8rem] lg:h-[7rem]">
             </div>
         </div>
+        <div class="md:h-[10%]"></div>
     </div>
 </div>
 
