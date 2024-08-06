8fa71c5127df2cfc7571abd3e6815eec16b88e5d
Filipe Lemes Ramalho <53817868+MomoiroAkachan@users.noreply.github.com>
2024-08-06 20:18:34 -0300

    Update base.blade.php

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0">
     <meta name="description" content="@yield('description')">
-    <title> {{ config('app.name') }}  - @yield('title')</title>
+    <title> Formulário de Cadastro Escolar  - @yield('title')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @yield('styles')
 </head>
