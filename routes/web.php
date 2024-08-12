678e18ee1655116b9312df89e38b9178eb89db96
MomoiroAkachan <filiplemes@hotmail.com>
2024-08-12 10:05:38 -0300

    feat(middleware): removes middleware controll on routes

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,13 @@ use App\Http\Controllers\FormController;
 use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(LandingController::class)->group(function () {
+// Basic Application Routes
+Route::controller(LandingController::class)->group(function(){
     Route::get('/', 'index')->name('home');
-    Route::get('/coming', 'coming')->name('coming');
 });
 
-
-
-
-Route::controller(FormController::class)->group(function () {
-    Route::get('/form', 'index')->name('form.index');
+// Form routes
+Route::controller(FormController::class)->group(function(){
+    Route::get('form', 'index')->name('form.index');
+    Route::get('form/{page}', 'page')->name('form.page');
 });
-
